08349eba516ef783b0c5b7a7592eed6895966fbd
MilanMarakana <milan.marakana7@gmail.com>
2022-06-16 17:19:57 +0200

    update tailwind cdn and completed home-page and pizza menu page manully

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-import express from 'express';
-import ejs from 'ejs';
-import expressLayouts from 'express-ejs-layouts';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import express from 'express';
+const express = require('express');
+// import ejs from 'ejs';
+const ejs = require('ejs');
+// import expressLayouts from 'express-ejs-layouts';
+const expressLayouts = require('express-ejs-layouts');
+// import path from 'path';
+const path = require('path');
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//Assests
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('home');
